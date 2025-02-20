49573779b5b9eb7b0fa2c9b5a2271f782418714e
ruchit82 <ruchitsanap00@gmail.com>
2025-02-20 13:16:14 +0530

    Update inventory_mangement.py

--- inventory_mangement.py
+++ inventory_mangement.py
@@ -141,7 +141,7 @@ elif page == "Reports":
         msg['Subject'] = subject
         msg.attach(MIMEText(body, 'plain'))
         
-        with smtplib.SMTP('smtp.example.com', 587) as server:
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
             server.starttls()
             server.login(sender_email, "your_password")
             server.sendmail(sender_email, receiver_email, msg.as_string())
